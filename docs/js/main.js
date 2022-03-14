766a9f0e5a223d8fb8ef88814b16a2ca0a360771
WillianAbrego <willianantonioabrego@gmail.com>
2022-03-14 10:19:59 -0600

    div html

---
+++ docs/js/main.js
@@ -0,0 +1,9 @@
+const ratingContainer = document.querySelector(".rating");
+let currentValue = 0;
+const limit = 5;
+
+const html = Array.from(Array(limit)).map((_, i) => {
+  return `<div class="item item-${i}" data-pos=${i}></div>`;
+});
+
+ratingContainer.innerHTML = html.join("");
